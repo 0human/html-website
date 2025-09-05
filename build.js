a7babace35cc808267100c3eb99b154f0949ea34
0humanbuilder <0human.builder@gmail.com>
2025-09-05 10:28:05 +0800

    feat: add commit link

--- build.js
+++ build.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 // 读取环境变量
-const branch = process.env.PUBLIC_BRANCH || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF;
+const branch = process.env.PUBLIC_BRANCH || process.env.VERCEL_GIT_COMMIT_REF;
 const version = process.env.PUBLIC_VERSION || `latest-${branch}`;
-const commitSha = process.env.PUBLIC_COMMIT_SHA || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+const commitSha = process.env.PUBLIC_COMMIT_SHA || process.env.VERCEL_GIT_COMMIT_SHA;
 
 // 读取 HTML 模板
 const htmlPath = path.join(__dirname, 'index.html');
@@ -13,7 +13,7 @@ let htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
 // 替换占位符
 htmlContent = htmlContent
-  .replace(/{{PUBLIC_COMMIT_SHA}}/g, commitSha)
+  .replace(/{{PUBLIC_COMMIT_SHA}}/g, commitSha.slice(0, 7))
   .replace(/{{PUBLIC_VERSION}}/g, version);
 
 // 写入最终 HTML
